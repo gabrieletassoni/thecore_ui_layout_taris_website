619f5f9017c903c2770769a268af111ad1ac4fe6
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2018-01-31 22:42:04 +0100

    Added static assets manager

--- lib/thecore_ui_layout_taris_website/engine.rb
+++ lib/thecore_ui_layout_taris_website/engine.rb
@@ -1,4 +1,11 @@
 module ThecoreUiLayoutTarisWebsite
   class Engine < ::Rails::Engine
+    # This code makes the static assets present in this 
+    # engine available to main app, even if this gem is 
+    # just a dependency of another engine
+    initializer 'thecore_ui_layout_taris_website.load_static_assets' do |app|
+      puts "Loading static assets for #{root}"
+      app.middleware.use ::ActionDispatch::Static, "#{root}/app"
+    end
   end
 end
--- lib/thecore_ui_layout_taris_website/version.rb
+++ lib/thecore_ui_layout_taris_website/version.rb
@@ -1,3 +1,3 @@
 module ThecoreUiLayoutTarisWebsite
-  VERSION = '1.1.1'
+  VERSION = '1.1.4'
 end
